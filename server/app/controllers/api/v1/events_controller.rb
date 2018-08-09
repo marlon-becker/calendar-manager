23b346645e1860cae04324a312d4882d70e4c4b3
marlon <marlon.mbs@gmail.com>
2018-08-09 12:23:55 +0200

    chore: add methods in the event controller to get the full list of events, get single event and create event

---
+++ server/app/controllers/api/v1/events_controller.rb
@@ -0,0 +1,29 @@
+module Api
+  module V1
+    class EventsController < ApplicationController
+      def index
+        @events = Event.order('event_time ASC');
+        render json: {status:'SUCCESS', message:'Loaded events', data:@events}, status: :ok
+      end
+
+      def show
+        @event = Event.find(params[:id])
+        render json: {status:'SUCCESS', message:'Loaded event', data:@event}, status: :ok
+      end
+
+      def create
+        event = Event.new(event_params)
+        if event.save
+          render json: {status:'SUCCESS', message:'Saved event', data:event}, status: :ok
+        else
+          render json: {status:'ERROR', message:'Event not saved', data:event.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+      def event_params
+        params.permit(:title, :description, :event_time, :location)
+      end
+    end
+  end
+end
