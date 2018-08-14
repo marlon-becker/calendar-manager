309e67c3ffbb822c497bf89af9c4a33edc5bf56b
marlon <marlon.mbs@gmail.com>
2018-08-14 12:29:17 +0200

    chore: move the token controller to correct namespace

--- server/app/controllers/api/v1/user_token_controller.rb
+++ server/app/controllers/api/v1/user_token_controller.rb
@@ -1,3 +1,7 @@
-class UserTokenController < Knock::AuthTokenController
-  skip_before_action :verify_authenticity_token, raise: false
+module Api
+  module V1
+    class UserTokenController < Knock::AuthTokenController
+      skip_before_action :verify_authenticity_token, raise: false
+    end
+  end
 end
