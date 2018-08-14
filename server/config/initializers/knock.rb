f30dbb5c66ce7f0a6c569812e085bd1ec29c9ec2
marlon <marlon.mbs@gmail.com>
2018-08-14 16:00:43 +0200

    fix: change how secret_key_base is used

--- server/config/initializers/knock.rb
+++ server/config/initializers/knock.rb
@@ -1,9 +1,9 @@
 Knock.setup do |config|
-
+  puts Rails.application
   # Set how long a login token is valid.
   config.token_lifetime = 1.week
   config.token_signature_algorithm = 'HS256'
-  config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
+  config.token_secret_signature_key = -> { Rails.application.secret_key_base }
   config.not_found_exception_class_name = 'ActiveRecord::RecordNotFound'
 
 end
